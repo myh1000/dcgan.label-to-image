367b713eb330f86617315354ad98e7b0d67efd5f
myh1000 <myh1000@gmail.com>
2017-03-09 01:03:52 -0800

    ops and utils from carpedm20

--- utils.py
+++ utils.py
@@ -1,69 +1,87 @@
+"""
+Some codes from https://github.com/Newmu/dcgan_code
+"""
+from __future__ import division
 import math
+import json
+import random
+import pprint
+import scipy.misc
 import numpy as np
-import tensorflow as tf
+from time import gmtime, strftime
+from six.moves import xrange
 
-class batch_norm(object):
-            # h1 = lrelu(tf.contrib.layers.batch_norm(conv2d(h0, self.df_dim*2, name='d_h1_conv'),decay=0.9,updates_collections=None,epsilon=0.00001,scale=True,scope="d_h1_conv"))
-    def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
-        with tf.variable_scope(name):
-            self.epsilon = epsilon
-            self.momentum = momentum
-            self.name = name
+pp = pprint.PrettyPrinter()
 
-    def __call__(self, x, train=True):
-        return tf.contrib.layers.batch_norm(x, decay=self.momentum, updates_collections=None, epsilon=self.epsilon, scale=True, scope=self.name)
+get_stddev = lambda x, k_h, k_w: 1/math.sqrt(k_w*k_h*x.get_shape()[-1])
 
-def conv2d(input_, output_dim,
-           k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
-           name="conv2d"):
-    with tf.variable_scope(name):
-        w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
-                            initializer=tf.truncated_normal_initializer(stddev=stddev))
-        conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
+def get_image(image_path, input_height, input_width,
+              resize_height=64, resize_width=64,
+              is_crop=True, is_grayscale=False):
+  image = imread(image_path, is_grayscale)
+  return transform(image, input_height, input_width,
+                   resize_height, resize_width, is_crop)
 
-        biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
-        conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+def save_images(images, size, image_path):
+  return imsave(inverse_transform(images), size, image_path)
 
-        return conv
+def imread(path, is_grayscale = False):
+  if (is_grayscale):
+    return scipy.misc.imread(path, flatten = True).astype(np.float)
+  else:
+    return scipy.misc.imread(path).astype(np.float)
 
-def deconv2d(input_, output_shape,
-             k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
-             name="deconv2d", with_w=False):
-    with tf.variable_scope(name):
-        # filter : [height, width, output_channels, in_channels]
-        w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
-                            initializer=tf.random_normal_initializer(stddev=stddev))
+def merge_images(images, size):
+  return inverse_transform(images)
 
-        try:
-            deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
-                                strides=[1, d_h, d_w, 1])
+def merge(images, size):
+  h, w = images.shape[1], images.shape[2]
+  img = np.zeros((h * size[0], w * size[1], 3))
+  for idx, image in enumerate(images):
+    i = idx % size[1]
+    j = idx // size[1]
+    img[j*h:j*h+h, i*w:i*w+w, :] = image
+  return img
 
-        # Support for verisons of TensorFlow before 0.7.0
-        except AttributeError:
-            deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
-                                strides=[1, d_h, d_w, 1])
+def imsave(images, size, path):
+  return scipy.misc.imsave(path, merge(images, size))
 
-        biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
-        deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+def center_crop(x, crop_h, crop_w,
+                resize_h=64, resize_w=64):
+  if crop_w is None:
+    crop_w = crop_h
+  h, w = x.shape[:2]
+  j = int(round((h - crop_h)/2.))
+  i = int(round((w - crop_w)/2.))
+  return scipy.misc.imresize(
+      x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
 
-        if with_w:
-            return deconv, w, biases
-        else:
-            return deconv
+def transform(image, input_height, input_width,
+              resize_height=64, resize_width=64, is_crop=True):
+  if is_crop:
+    cropped_image = center_crop(
+      image, input_height, input_width,
+      resize_height, resize_width)
+  else:
+    cropped_image = scipy.misc.imresize(image, [resize_height, resize_width])
+  return np.array(cropped_image)/127.5 - 1.
 
+def inverse_transform(images):
+  return (images+1.)/2.
 
-def lrelu(x, leak=0.2, name="lrelu"):
-  return tf.maximum(x, leak*x)
+def make_gif(images, fname, duration=2, true_image=False):
+  import moviepy.editor as mpy
 
-def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False):
-    shape = input_.get_shape().as_list()
+  def make_frame(t):
+    try:
+      x = images[int(len(images)/duration*t)]
+    except:
+      x = images[-1]
 
-    with tf.variable_scope(scope or "Linear"):
-        matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
-                                 tf.random_normal_initializer(stddev=stddev))
-        bias = tf.get_variable("bias", [output_size],
-            initializer=tf.constant_initializer(bias_start))
-        if with_w:
-            return tf.matmul(input_, matrix) + bias, matrix, bias
-        else:
-            return tf.matmul(input_, matrix) + bias
+    if true_image:
+      return x.astype(np.uint8)
+    else:
+      return ((x+1)/2*255).astype(np.uint8)
+
+  clip = mpy.VideoClip(make_frame, duration=duration)
+  clip.write_gif(fname, fps = len(images) / duration)
