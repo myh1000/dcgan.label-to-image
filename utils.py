259f20eaff8307e232919d5fc8ac72e92f24d410
myh1000 <myh1000@gmail.com>
2017-03-11 00:35:50 -0800

    more utils

--- utils.py
+++ utils.py
@@ -5,83 +5,94 @@ from __future__ import division
 import math
 import json
 import random
-import pprint
 import scipy.misc
 import numpy as np
 from time import gmtime, strftime
 from six.moves import xrange
-
-pp = pprint.PrettyPrinter()
+import fire
 
 get_stddev = lambda x, k_h, k_w: 1/math.sqrt(k_w*k_h*x.get_shape()[-1])
 
 def get_image(image_path, input_height, input_width,
               resize_height=64, resize_width=64,
               is_crop=True, is_grayscale=False):
-  image = imread(image_path, is_grayscale)
-  return transform(image, input_height, input_width,
+    image = imread(image_path, is_grayscale)
+    return transform(image, input_height, input_width,
                    resize_height, resize_width, is_crop)
 
-def save_images(images, size, image_path):
-  return imsave(inverse_transform(images), size, image_path)
+def save_images(images, size, image_path, is_grayscale=False):
+    return imsave(inverse_transform(images), size, image_path, is_grayscale)
 
-def imread(path, is_grayscale = False):
-  if (is_grayscale):
-    return scipy.misc.imread(path, flatten = True).astype(np.float)
-  else:
-    return scipy.misc.imread(path).astype(np.float)
-
-def merge_images(images, size):
-  return inverse_transform(images)
+def imread(path, is_grayscale=False):
+    if (is_grayscale):
+        return scipy.misc.imread(path, flatten = True).astype(np.float)
+    else:
+        return scipy.misc.imread(path).astype(np.float)
 
 def merge(images, size):
-  h, w = images.shape[1], images.shape[2]
-  img = np.zeros((h * size[0], w * size[1], 3))
-  for idx, image in enumerate(images):
-    i = idx % size[1]
-    j = idx // size[1]
-    img[j*h:j*h+h, i*w:i*w+w, :] = image
-  return img
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1], 3))
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx // size[1]
+        img[j*h:j*h+h, i*w:i*w+w, :] = image
+    return img
 
-def imsave(images, size, path):
-  return scipy.misc.imsave(path, merge(images, size))
+def merge_gray(images, size):
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1]))
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx // size[1]
+        img[j*h:j*h+h, i*w:i*w+w] = image
+    return img
+
+def imsave(images, size, path, is_grayscale=False):
+    if (is_grayscale):
+        return scipy.misc.imsave(path, merge_gray(images, size))
+    else:
+        return scipy.misc.imsave(path, merge(images, size))
 
 def center_crop(x, crop_h, crop_w,
                 resize_h=64, resize_w=64):
-  if crop_w is None:
-    crop_w = crop_h
-  h, w = x.shape[:2]
-  j = int(round((h - crop_h)/2.))
-  i = int(round((w - crop_w)/2.))
-  return scipy.misc.imresize(
-      x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
+    if crop_w is None:
+        crop_w = crop_h
+        h, w = x.shape[:2]
+        j = int(round((h - crop_h)/2.))
+        i = int(round((w - crop_w)/2.))
+    return scipy.misc.imresize(
+        x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
 
 def transform(image, input_height, input_width,
               resize_height=64, resize_width=64, is_crop=True):
-  if is_crop:
-    cropped_image = center_crop(
-      image, input_height, input_width,
-      resize_height, resize_width)
-  else:
-    cropped_image = scipy.misc.imresize(image, [resize_height, resize_width])
-  return np.array(cropped_image)/127.5 - 1.
+    if is_crop:
+        cropped_image = center_crop(
+          image, input_height, input_width,
+          resize_height, resize_width)
+    else:
+        cropped_image = scipy.misc.imresize(image, [resize_height, resize_width])
+    return np.array(cropped_image)/127.5 - 1.
+  # return cropped_image
 
 def inverse_transform(images):
-  return (images+1.)/2.
+    return (images+1.)/2.
 
 def make_gif(images, fname, duration=2, true_image=False):
-  import moviepy.editor as mpy
+    import moviepy.editor as mpy
 
-  def make_frame(t):
-    try:
-      x = images[int(len(images)/duration*t)]
-    except:
-      x = images[-1]
+    def make_frame(t):
+        try:
+            x = images[int(len(images)/duration*t)]
+        except:
+            x = images[-1]
 
-    if true_image:
-      return x.astype(np.uint8)
-    else:
-      return ((x+1)/2*255).astype(np.uint8)
+        if true_image:
+            return x.astype(np.uint8)
+        else:
+            return ((x+1)/2*255).astype(np.uint8)
 
-  clip = mpy.VideoClip(make_frame, duration=duration)
-  clip.write_gif(fname, fps = len(images) / duration)
+    clip = mpy.VideoClip(make_frame, duration=duration)
+    clip.write_gif(fname, fps = len(images) / duration)
+
+
+fire.Fire()
