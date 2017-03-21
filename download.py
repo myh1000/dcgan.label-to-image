c273767cdfb1ebf8c062514bb2d30c6ae75c0f54
myh1000 <myh1000@gmail.com>
2017-03-20 23:49:20 -0700

    retry for urlopen if timeout

--- download.py
+++ download.py
@@ -7,15 +7,23 @@ import untangle
 import scipy.misc
 from google.cloud import storage
 import tempfile
+from decorator import retry
 from preprocess import *
+
+@retry(urllib2.URLError, tries=4, delay=3, backoff=2)
+def readURL(tag, i):
+    return urllib2.urlopen("http://safebooru.org/index.php?page=dapi&s=post&q=index&tags=1girl+"+tag+"&pid="+str(i+20)).read()
 
 def run(tag_classes, start_count):
 
-    temp = tempfile.NamedTemporaryFile()
+    if not os.path.exists(tag_classes[0]):
+        os.makedirs(tag_classes[0])
+    temp = tempfile.NamedTemporaryFile(dir=tag_classes[0])
     client = storage.Client()
     bucket = client.get_bucket("dcgan-161707-mlengine")
 
     count = int(start_count)
+    fail = 0
     maxsize = 512
 
     # add glasses + hair color / combos
@@ -30,7 +38,7 @@ def run(tag_classes, start_count):
     for idx, tag in enumerate(tag_classes):
         print("Now downloading... " + tag)
         for i in xrange(10):
-            stringreturn = urllib2.urlopen("http://safebooru.org/index.php?page=dapi&s=post&q=index&tags=1girl+"+tag+"&pid="+str(i+20)).read()
+            stringreturn = readURL(tag, i)
             xmlreturn = untangle.parse(stringreturn)
             for post in xmlreturn.posts.post:
                 imgurl = "http:" + post["sample_url"]
@@ -40,16 +48,19 @@ def run(tag_classes, start_count):
                     image = np.asarray(bytearray(resp.read()), dtype="uint8")
                     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-                    count += 1
                     temp.seek(0,0)
-                    scipy.misc.imsave(temp,image, "jpeg")
-                    blob = bucket.blob("imgs/"+str(count)+".jpg")
+                    scipy.misc.imsave(temp,image[...,::-1], "jpeg")
                     temp.seek(0,0)
                     if process_img(temp.name) != 0:
+                        count += 1
+                        blob = bucket.blob("imgs/"+str(count)+".jpg")
                         blob.upload_from_file(temp,content_type='image/jpeg')
                         imgs.append("imgs/"+str(count)+".jpg")
                         tags.append(idx)
                         tagname.append(tag)
+                    else:
+                        fail += 1
+    print("Done: %d failed" % fail)
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
