02281c33abecbe99ecf467f0e282dbb37f8172fa
myh1000 <myh1000@gmail.com>
2017-03-22 22:45:46 -0700

    cgan or dcgan choices

--- main.py
+++ main.py
@@ -46,21 +46,27 @@ class txt2pic():
         # batch normalization : deals with poor initialization helps gradient flow
         self.d_bn1 = batch_norm(name='d_bn1')
         self.d_bn2 = batch_norm(name='d_bn2')
+        self.d_bn3 = batch_norm(name='d_bn3')
 
         self.g_bn0 = batch_norm(name='g_bn0')
         self.g_bn1 = batch_norm(name='g_bn1')
         self.g_bn2 = batch_norm(name='g_bn2')
+        self.g_bn3 = batch_norm(name='g_bn3')
 
         self.checkpoint_dir = "./checkpoint"
         self.build_model()
 
     def build_model(self):
-        self.tags = tf.placeholder(tf.float32, [self.batch_size, self.y_dim], name='tags')
+        self.tags = tf.placeholder(tf.float32, [self.batch_size, self.y_dim], name='tags') # comment out if not cGAN
         image_dims = [self.image_size, self.image_size, self.c_dim]
 
         self.inputs = tf.placeholder(tf.float32, [self.batch_size] + image_dims, name='real_images')
         self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
 
+        # self.G = self.generator(self.z)
+        # self.sampler = self.sampler(self.z)
+        # self.D, self.D_logits = self.discriminator(self.inputs, reuse=False) # Real
+        # self.D_, self.D_logits_ = self.discriminator(self.G, reuse=True)     # Fake
         self.G = self.generator(self.z, self.tags)
         self.sampler = self.sampler(self.z, self.tags)
         self.D, self.D_logits = self.discriminator(self.inputs, self.tags, reuse=False) # Real
@@ -90,6 +96,7 @@ class txt2pic():
         # data = glob(os.path.join("imgs", "*.jpg"))  # YOUR OWN DATASET create a folder "/imgs"
         data = glob(os.path.join("birds", "*.jpg"))[:542]  #CUB BIRD DATASET -- download and put first 10 class birds into "/birds"
         tags = np.zeros((543, self.y_dim), dtype=np.float32)
+        # for each class type
         tags[:59, 0] = 1
         tags[59:118, 1] = 1
         tags[118:176, 2] = 1
@@ -101,49 +108,12 @@ class txt2pic():
         tags[422:481, 8] = 1
         tags[481:541, 9] = 1
         p = np.random.permutation(543)
-        data = data[p]
+        data = np.array(data)[p]
         tags = tags[p]
-
-        # IF MNIST
-        # data_dir = os.path.join("./data", "")
-        #
-        # fd = open(os.path.join(data_dir,'train-images-idx3-ubyte'))
-        # loaded = np.fromfile(file=fd,dtype=np.uint8)
-        # trX = loaded[16:].reshape((60000,28,28,1)).astype(np.float)
-        #
-        # fd = open(os.path.join(data_dir,'train-labels-idx1-ubyte'))
-        # loaded = np.fromfile(file=fd,dtype=np.uint8)
-        # trY = loaded[8:].reshape((60000)).astype(np.float)
-        #
-        # fd = open(os.path.join(data_dir,'t10k-images-idx3-ubyte'))
-        # loaded = np.fromfile(file=fd,dtype=np.uint8)
-        # teX = loaded[16:].reshape((10000,28,28,1)).astype(np.float)
-        #
-        # fd = open(os.path.join(data_dir,'t10k-labels-idx1-ubyte'))
-        # loaded = np.fromfile(file=fd,dtype=np.uint8)
-        # teY = loaded[8:].reshape((10000)).astype(np.float)
-        #
-        # trY = np.asarray(trY)
-        # teY = np.asarray(teY)
-        #
-        # X = np.concatenate((trX, teX), axis=0)
-        # y = np.concatenate((trY, teY), axis=0).astype(np.int)
-        #
-        # seed = 547
-        # np.random.seed(seed)
-        # np.random.shuffle(X)
-        # np.random.seed(seed)
-        # np.random.shuffle(y)
-        #
-        # y_vec = np.zeros((len(y), self.y_dim), dtype=np.float)
-        # for i, label in enumerate(y):
-        #   y_vec[i,y[i]] = 1.0
-        #
-        # data = X/255.
-        # tags = y_vec
 
         if not os.path.exists("./samples"):
             os.makedirs("./samples")
+
         counter = 0
 
         if could_load:
@@ -153,11 +123,11 @@ class txt2pic():
             print(" [!] Load failed...")
 
         sample = np.array([
-                get_image(batch_file,
+                self.sess.run(tf.image.random_flip_left_right(get_image(batch_file,
                     input_height=self.image_size,
                     input_width=self.image_size,
                     resize_height=self.output_size,
-                    resize_width=self.output_size) for batch_file in data[0:self.batch_size]])
+                    resize_width=self.output_size))) for batch_file in data[0:self.batch_size]])
         save_images(sample, [int(math.sqrt(self.batch_size)), int(math.sqrt(self.batch_size))], "samples/training_ex.png")
         sample_inputs = np.array(sample).astype(np.float32)[:, :, :, :3]
         sample_z = np.random.uniform(-1, 1, size=(self.batch_size , self.z_dim))
@@ -169,11 +139,11 @@ class txt2pic():
             for idx in xrange(batch_idxs):
                 batch_images = data[idx*self.batch_size:(idx+1)*self.batch_size]
                 batch = [
-                    get_image(batch_file,
+                    self.sess.run(tf.image.random_flip_left_right(get_image(batch_file,
                         input_height=self.image_size,
                         input_width=self.image_size,
                         resize_height=self.output_size,
-                        resize_width=self.output_size) for batch_file in batch_images]
+                        resize_width=self.output_size))) for batch_file in batch_images]
                 batch_images = np.array(batch).astype(np.float32)
                 batch_tags = tags[idx*self.batch_size:(idx+1)*self.batch_size]
                 batch_z = np.random.uniform(-1, 1, [self.batch_size, self.z_dim]).astype(np.float32) # noise
@@ -190,7 +160,8 @@ class txt2pic():
                 # errG = self.g_loss.eval({self.real_data: batch_images})
 
                 counter += 1
-                print("Epoch: [%2d] [%4d/%4d], d_loss: %.8f, g_loss: %.8f" \
+                if np.mod(counter, 10) == 1:  # log every 10 iters
+                    print("Epoch: [%2d] [%4d/%4d], d_loss: %.8f, g_loss: %.8f" \
                         % (epoch, idx+1, batch_idxs, errD, errG))
 
                 if np.mod(counter, 10) == 1:
@@ -207,78 +178,144 @@ class txt2pic():
                 if np.mod(counter, 500) == 2 or np.mod(idx+5, batch_idxs) == 1:
                   self.save(self.checkpoint_dir, counter)
 
-    def generator(self, z, tags):
+    def generator(self, z, tags=None):
         with tf.variable_scope("generator") as scope:
-            s_h, s_w = self.output_size, self.output_size
-            s_h2, s_h4 = int(s_h/2), int(s_h/4)
-            s_w2, s_w4 = int(s_w/2), int(s_w/4)
+            if tags is None:
+                s_h, s_w = self.output_size, self.output_size
+                s_h2, s_w2 = conv_out_size_same(s_h, 2), conv_out_size_same(s_w, 2)
+                s_h4, s_w4 = conv_out_size_same(s_h2, 2), conv_out_size_same(s_w2, 2)
+                s_h8, s_w8 = conv_out_size_same(s_h4, 2), conv_out_size_same(s_w4, 2)
+                s_h16, s_w16 = conv_out_size_same(s_h8, 2), conv_out_size_same(s_w8, 2)
 
-            yb = tf.reshape(tags, [self.batch_size, 1, 1, self.y_dim])
-            z = tf.concat(values=[z, tags], axis=1)
+                # project `z` and reshape
+                self.z_, self.h0_w, self.h0_b = linear(
+                    z, self.gf_dim*8*s_h16*s_w16, 'g_h0_lin', with_w=True)
 
-            h0 = tf.nn.relu(
-                self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin')))
-            h0 = tf.concat(values=[h0, tags], axis=1)
+                self.h0 = tf.reshape(
+                    self.z_, [-1, s_h16, s_w16, self.gf_dim * 8])
+                h0 = tf.nn.relu(self.g_bn0(self.h0))
 
-            h1 = tf.nn.relu(self.g_bn1(
-                linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin')))
-            h1 = tf.reshape(h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
+                self.h1, self.h1_w, self.h1_b = deconv2d(
+                    h0, [self.batch_size, s_h8, s_w8, self.gf_dim*4], name='g_h1', with_w=True)
+                h1 = tf.nn.relu(self.g_bn1(self.h1))
 
-            h1 = conv_cond_concat(h1, yb)
+                h2, self.h2_w, self.h2_b = deconv2d(
+                    h1, [self.batch_size, s_h4, s_w4, self.gf_dim*2], name='g_h2', with_w=True)
+                h2 = tf.nn.relu(self.g_bn2(h2))
 
-            h2 = tf.nn.relu(self.g_bn2(deconv2d(h1,
-                [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2')))
-            h2 = conv_cond_concat(h2, yb)
+                h3, self.h3_w, self.h3_b = deconv2d(
+                    h2, [self.batch_size, s_h2, s_w2, self.gf_dim*1], name='g_h3', with_w=True)
+                h3 = tf.nn.relu(self.g_bn3(h3))
 
-            return tf.nn.sigmoid(deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
+                h4, self.h4_w, self.h4_b = deconv2d(
+                    h3, [self.batch_size, s_h, s_w, self.c_dim], name='g_h4', with_w=True)
 
-    def discriminator(self, image, tags, reuse=False):
+                return tf.nn.tanh(h4)
+            else:
+                s_h, s_w = self.output_size, self.output_size
+                s_h2, s_h4 = int(s_h/2), int(s_h/4)
+                s_w2, s_w4 = int(s_w/2), int(s_w/4)
+                yb = tf.reshape(tags, [self.batch_size, 1, 1, self.y_dim])
+                z = tf.concat(values=[z, tags], axis=1)
+
+                h0 = tf.nn.relu(
+                    self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin')))
+                h0 = tf.concat(values=[h0, tags], axis=1)
+
+                h1 = tf.nn.relu(self.g_bn1(
+                    linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin')))
+                h1 = tf.reshape(h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
+
+                h1 = conv_cond_concat(h1, yb)
+
+                h2 = tf.nn.relu(self.g_bn2(deconv2d(h1,
+                    [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2')))
+                h2 = conv_cond_concat(h2, yb)
+
+                return tf.nn.sigmoid(deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
+
+    def discriminator(self, image, tags=None, reuse=False):
         with tf.variable_scope("discriminator") as scope:
             if reuse:
                 tf.get_variable_scope().reuse_variables()
             else:
                 assert tf.get_variable_scope().reuse == False
 
-            yb = tf.reshape(tags, [self.batch_size, 1, 1, self.y_dim])
-            x = conv_cond_concat(image, yb)
+            f tags is None:
+                h0 = lrelu(conv2d(image, self.df_dim, name='d_h0_conv'))
+                h1 = tf.nn.dropout(lrelu(self.d_bn1(conv2d(h0, self.df_dim*2, name='d_h1_conv'))),0.5)
+                h2 = tf.nn.dropout(lrelu(self.d_bn2(conv2d(h1, self.df_dim*4, name='d_h2_conv'))),0.5)
+                h3 = lrelu(self.d_bn3(conv2d(h2, self.df_dim*8, name='d_h3_conv')))
+                h4 = linear(tf.reshape(h3, [self.batch_size, -1]), 1, 'd_h3_lin')
 
-            h0 = lrelu(conv2d(x, self.c_dim + self.y_dim, name='d_h0_conv'))
-            h0 = conv_cond_concat(h0, yb)
+                return tf.nn.sigmoid(h4), h4
+            else:
+                yb = tf.reshape(tags, [self.batch_size, 1, 1, self.y_dim])
+                x = conv_cond_concat(image, yb)
 
-            h1 = lrelu(self.d_bn1(conv2d(h0, self.df_dim + self.y_dim, name='d_h1_conv')))
-            h1 = tf.reshape(h1, [self.batch_size, -1])
-            h1 = tf.concat(values=[h1, tags], axis=1)
+                h0 = lrelu(conv2d(x, self.c_dim + self.y_dim, name='d_h0_conv'))
+                h0 = conv_cond_concat(h0, yb)
 
-            h2 = lrelu(self.d_bn2(linear(h1, self.dfc_dim, 'd_h2_lin')))
-            h2 = tf.concat(values=[h2, tags], axis=1)
+                h1 = lrelu(self.d_bn1(conv2d(h0, self.df_dim + self.y_dim, name='d_h1_conv')))
+                h1 = tf.reshape(h1, [self.batch_size, -1])
+                h1 = tf.concat(values=[h1, tags], axis=1)
 
-            h3 = linear(h2, 1, 'd_h3_lin')
+                h2 = lrelu(self.d_bn2(linear(h1, self.dfc_dim, 'd_h2_lin')))
+                h2 = tf.concat(values=[h2, tags], axis=1)
 
-            return tf.nn.sigmoid(h3), h3
+                h3 = linear(h2, 1, 'd_h3_lin')
 
-    def sampler(self, z, tags):
+                return tf.nn.sigmoid(h3), h3
+
+    def sampler(self, z, tags=None):
         with tf.variable_scope("generator") as scope:
             scope.reuse_variables()
 
-            s_h, s_w = self.output_size, self.output_size
-            s_h2, s_h4 = int(s_h/2), int(s_h/4)
-            s_w2, s_w4 = int(s_w/2), int(s_w/4)
+            if tags is None:
+                s_h, s_w = self.output_size, self.output_size
+                s_h2, s_w2 = conv_out_size_same(s_h, 2), conv_out_size_same(s_w, 2)
+                s_h4, s_w4 = conv_out_size_same(s_h2, 2), conv_out_size_same(s_w2, 2)
+                s_h8, s_w8 = conv_out_size_same(s_h4, 2), conv_out_size_same(s_w4, 2)
+                s_h16, s_w16 = conv_out_size_same(s_h8, 2), conv_out_size_same(s_w8, 2)
 
-            yb = tf.reshape(tags, [self.batch_size, 1, 1, self.y_dim])
-            z = tf.concat([z, tags], 1)
+                # project `z` and reshape
+                h0 = tf.reshape(
+                    linear(z, self.gf_dim*8*s_h16*s_w16, 'g_h0_lin'),
+                    [-1, s_h16, s_w16, self.gf_dim * 8])
+                h0 = tf.nn.relu(self.g_bn0(h0, train=False))
 
-            h0 = tf.nn.relu(self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin'), train=False))
-            h0 = tf.concat([h0, tags], 1)
+                h1 = deconv2d(h0, [self.batch_size, s_h8, s_w8, self.gf_dim*4], name='g_h1')
+                h1 = tf.nn.relu(self.g_bn1(h1, train=False))
 
-            h1 = tf.nn.relu(self.g_bn1(
-                linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin'), train=False))
-            h1 = tf.reshape(h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
-            h1 = conv_cond_concat(h1, yb)
+                h2 = deconv2d(h1, [self.batch_size, s_h4, s_w4, self.gf_dim*2], name='g_h2')
+                h2 = tf.nn.relu(self.g_bn2(h2, train=False))
 
-            h2 = tf.nn.relu(self.g_bn2(deconv2d(h1, [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2'), train=False))
-            h2 = conv_cond_concat(h2, yb)
+                h3 = deconv2d(h2, [self.batch_size, s_h2, s_w2, self.gf_dim*1], name='g_h3')
+                h3 = tf.nn.relu(self.g_bn3(h3, train=False))
 
-            return tf.nn.sigmoid(deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
+                h4 = deconv2d(h3, [self.batch_size, s_h, s_w, self.c_dim], name='g_h4')
+
+                return tf.nn.tanh(h4)
+            else:
+                s_h, s_w = self.output_size, self.output_size
+                s_h2, s_h4 = int(s_h/2), int(s_h/4)
+                s_w2, s_w4 = int(s_w/2), int(s_w/4)
+
+                yb = tf.reshape(tags, [self.batch_size, 1, 1, self.y_dim])
+                z = tf.concat([z, tags], 1)
+
+                h0 = tf.nn.relu(self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin'), train=False))
+                h0 = tf.concat([h0, tags], 1)
+
+                h1 = tf.nn.relu(self.g_bn1(
+                    linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin'), train=False))
+                h1 = tf.reshape(h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
+                h1 = conv_cond_concat(h1, yb)
+
+                h2 = tf.nn.relu(self.g_bn2(deconv2d(h1, [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2'), train=False))
+                h2 = conv_cond_concat(h2, yb)
+
+                return tf.nn.sigmoid(deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
 
 
     def save(self, checkpoint_dir, step):
